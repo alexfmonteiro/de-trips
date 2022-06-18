bf45adf748036500fa49c4b8d4e194e565f147b6
alexfmonteiro <fmonteiro.alex@gmail.com>
2022-06-18 10:47:45 -0300

    set log level

--- tests/test-spark.py
+++ tests/test-spark.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 # Spark session & context
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
+sc.setLogLevel("ERROR")
 
 # Sum of the first 100 whole numbers
 rdd = sc.parallelize(range(100 + 1))
